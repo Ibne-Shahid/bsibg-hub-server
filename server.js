b0fad2c2c77f0696e81347cf5f0fefc2d9adc2f4
Ibne-Shahid <anasbinshahid2004@gmail.com>
2026-03-06 00:56:33 +0600

    get and add user endpoint added

--- server.js
+++ server.js
@@ -4,19 +4,52 @@ const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
 
+const User = require('./models/User');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('BSIBG Database Connected!'))
-  .catch((err) => console.log('DB Connection Error:', err));
+.then(() => console.log('BSIBG Database Connected!'))
+.catch((err) => console.log('DB Connection Error:', err));
+
+app.get('/users', async (req, res) => {
+    const email = req.query.email;
+    try {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            return res.status(200).send({});
+        }
+        res.send(user);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
+app.post('/users', async (req, res) => {
+    const user = req.body;
+    try {
+        const query = { email: user.email };
+        const existingUser = await User.findOne(query);
+
+        if (existingUser) {
+            return res.send({ message: 'User already exists', user: existingUser });
+        }
+
+        const newUser = new User(user);
+        const result = await newUser.save();
+        res.status(201).send(result);
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
 
 app.get('/', (req, res) => {
-  res.send('BSIBG Server is Running...');
+    res.send('BSIBG Server is Running...');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server zooming on port ${PORT}`);
+    console.log(`Server zooming on port ${PORT}`);
 });
